d879b27ffafa8ee8be87005602ed1ce89adcc4f4
rbian <psychebb@163.com>
2014-11-13 19:12:41 +0800

    fake user info. bian & fang

--- facehub/app.py
+++ facehub/app.py
@@ -3,14 +3,14 @@ from decorators import json_response
 
 
 users_list = [
-    { "id": 1, "name": "Yao Shaobo", "photo":"http://uxhongkong.com/interviews/img/people/thumb-alain-robillard-bastien.jpg" },
-    { "id": 2, "name": "Dong Yuwei", "photo":"http://uxhongkong.com/interviews/img/people/thumb-andrew-mayfield.jpg" },
-    { "id": 3, "name": "Dou Yutao", "photo":"http://uxhongkong.com/interviews/img/people/thumb-boon-yew-chew.jpg" },
-    { "id": 4, "name": "Liu Liang", "photo":"http://uxhongkong.com/interviews/img/people/thumb-cornelius-rachieru.jpg" },
-    { "id": 1, "name": "Yao Shaobo", "photo":"http://uxhongkong.com/interviews/img/people/thumb-alain-robillard-bastien.jpg" },
-    { "id": 2, "name": "Dong Yuwei", "photo":"http://uxhongkong.com/interviews/img/people/thumb-andrew-mayfield.jpg" },
-    { "id": 3, "name": "Dou Yutao", "photo":"http://uxhongkong.com/interviews/img/people/thumb-boon-yew-chew.jpg" },
-    { "id": 4, "name": "Liu Liang", "photo":"http://uxhongkong.com/interviews/img/people/thumb-cornelius-rachieru.jpg" }
+    { "id": 1, "name": "Yao Shaobo", "photo":"http://uxhongkong.com/interviews/img/people/thumb-alain-robillard-bastien.jpg", "position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 2, "name": "Dong Yuwei", "photo":"http://uxhongkong.com/interviews/img/people/thumb-andrew-mayfield.jpg","position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 3, "name": "Dou Yutao", "photo":"http://uxhongkong.com/interviews/img/people/thumb-boon-yew-chew.jpg","position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 4, "name": "Liu Liang", "photo":"http://uxhongkong.com/interviews/img/people/thumb-cornelius-rachieru.jpg", "position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 1, "name": "Yao Shaobo", "photo":"http://uxhongkong.com/interviews/img/people/thumb-alain-robillard-bastien.jpg","position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 2, "name": "Dong Yuwei", "photo":"http://uxhongkong.com/interviews/img/people/thumb-andrew-mayfield.jpg", "position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 3, "name": "Dou Yutao", "photo":"http://uxhongkong.com/interviews/img/people/thumb-boon-yew-chew.jpg", "position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" },
+    { "id": 4, "name": "Liu Liang", "photo":"http://uxhongkong.com/interviews/img/people/thumb-cornelius-rachieru.jpg", "position": "MD", "project": "REA myfun.com", "email": "zh.li@thoughtworks.com", "phoneNumber": "13060245883", "skype": "david.xie" }
 ]
 
 data = {
