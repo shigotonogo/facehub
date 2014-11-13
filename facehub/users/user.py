f37ad42f175b16d95272359e947ef1d62a601f75
rbian <psychebb@163.com>
2014-11-13 19:20:40 +0800

    add rest properties in user.py. bian & liang

--- facehub/users/user.py
+++ facehub/users/user.py
@@ -10,9 +10,42 @@ class User(object):
 		self.__dict__['name']=value
 
 	@property
-	def department(self):
-		return self.__dict__['department']
+	def position(self):
+		return self.__dict__['position']
 
-	@department.setter
-	def department(self, value):
-		self.__dict__['department'] = value
+	@position.setter
+	def position(self, value):
+		self.__dict__['position'] = value
+
+	@property
+	def project(self):
+	    return self.__dict__['project']
+
+	@project.setter
+	def project(self, value):
+	    self.__dict__['project'] = value
+
+	@property
+	def email(self):
+	    return self.__dict__['email']
+	@email.setter
+	def email(self, value):
+	    self.__dict__['email'] = value
+
+	@property
+	def phoneNumber(self):
+	    return self.__dict__['phoneNumber']
+	@phoneNumber.setter
+	def phoneNumber(self, value):
+	    self.__dict__['phoneNumber'] = value
+
+	@property
+	def skype(self):
+	    return self.__dict__['skype']
+	@skype.setter
+	def skype(self, value):
+	    self.__dict__['skype'] = value
+	
+	
+	
+	
