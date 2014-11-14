4fcdbc02ba73c93a77ed485987043438650b4bce
Dou Yutao <yutao.dou@rea-group.com>
2014-11-14 11:29:21 +0800

    JS

--- public/assets/js/page/index.js
+++ public/assets/js/page/index.js
@@ -1,25 +1,36 @@
-(function(){
-  var popupCurrent = function(user_data) {
-    var current = new Ractive({template: $('#current-template').text() , data: user_data});
-    $.magnificPopup.open({
-      items:[{src: $(current.toHTML()), type: 'inline'}]
-    });
-  };
+(function() {
+    var popupCurrent = function(user_data) {
+        var current = new Ractive({
+            template: $('#current-template').text(),
+            data: user_data
+        });
+        $.magnificPopup.open({
+            items: [{
+                src: $(current.toHTML()),
+                type: 'inline'
+            }]
+        });
+    };
 
-  $('#members').delegate('.profile', 'click', function(src){
-    console.log(src.currentTarget);
+    $('#members').delegate('.profile', 'click', function(src) {
+        console.log(src.currentTarget);
+        $.ajax({
+            url: '/api/users/' + $(src.currentTarget).data(
+                'user-id'),
+            dataType: 'json',
+            success: popupCurrent
+        });
+    });
+
     $.ajax({
-      url: '/api/users/'+$(src.currentTarget).data('user-id'),
-      dataType: 'json',
-      success: popupCurrent
+        url: '/api/users',
+        dataType: 'json',
+        success: function(data) {
+            var ractive = new Ractive({
+                el: 'members',
+                template: '#template',
+                data: data
+            });
+        }
     });
-  });
-
-  $.ajax({
-    url: '/api/users',
-    dataType:'json',
-    success: function(data){
-      var ractive = new Ractive({el: 'members', template: '#template', data: data });
-    }
-  });
 })();
