0c854bb2192e472f8efaf40ac99f7c6fc0323903
Dou Yutao <yutao.dou@rea-group.com>
2014-12-11 19:00:35 +0800

    Upload img

--- services/api/qi_niu_api.py
+++ services/api/qi_niu_api.py
@@ -1,40 +1,22 @@
 import logging
 import os
-import qiniu.io
-import qiniu.conf
-import qiniu.rs
+from qiniu import Auth
+from qiniu import put_file
 import sys
-from facehub.settings import *
 
 class QiNiuAPi:
-    qiniu.conf.ACCESS_KEY = QINIU_ACCESS_KEY
-    qiniu.conf.SECRET_KEY = QINIU_SECRET_KEY
-    bucket_name = PROJECT_NAME
+    bucket_name = 'facehub'
+    mime_type = 'image/jpeg'
 
-    token=None
+    client = Auth('p0XvMhHCUOwTAUmdJdRvYVL58Set1kIEWQZDk5rF', 'PhFzLVRNFawZc9eM4fUPPyRlSTclientWkALmgxRvegkL')
 
     def upload(self, file_path):
-        uptoken = self.__get_token()
-        file_name = os.path.basename(file_path)
-        qiniu.rs.Client().delete(self.bucket_name, file_name)
-        localfile = "%s" % file_path
-        ret, err = qiniu.io.put_file(uptoken, file_name, localfile)
-        if err is not None:
-            logging.error('error: %s ' % err)
-            return
-        else:
-            return "http://facehub.qiniudn.com/%s" % file_name
+        key = os.path.basename(file_path)
+        token = self.client.upload_token(self.bucket_name, key)
+        ret, info = put_file(token, key, file_path)
 
-    def __get_token(self):
-        policy = qiniu.rs.PutPolicy('facehub')
-        return policy.token()   
+        print(ret)
+        print(info)
 
 if __name__ == '__main__':
-    QiNiuAPi().upload('./tmp/test.png')
-
-
-
-
-
-
-
+    QiNiuAPi().upload('/tmp/testimg.jpg')
