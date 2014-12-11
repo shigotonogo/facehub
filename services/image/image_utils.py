4c6d68734e43252fe6517217eb56d7d3bc7069c8
Dou Yutao <yutao.dou@rea-group.com>
2014-12-11 17:36:59 +0800

    Run

--- services/image/image_utils.py
+++ services/image/image_utils.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import io
-import urllib.request
+import urllib
 
 def crop_image(image_src, x, y, width, height, scale = 1):
-    image = Image.open(io.BytesIO(urllib.request.urlopen(image_src).read()))
+    image = Image.open(io.BytesIO(urllib.urlopen(image_src).read()))
     image.crop((int(x), int(y), int(x) + int(width), int(y) + int(height)))
     image.resize((int(width) * scale, int(height) * scale), Image.NEAREST)
     tmpImage= "/tmp%s" % uuid.uuid4()
