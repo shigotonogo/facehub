778c64c36954d40a0577fbbce1ee3d416ee5ca6e
He Fei <droxer.he@gmail.com>
2014-12-14 18:50:45 +0800

    make the all works against mysql.

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
+PYTHON3=python
 default: deps test
 
 deps:
-	pip install tox
+	pip install -r requirements.txt
 
 test:
 	tox
+
+run: deps
+	$(PYTHON3) -V
+	$(PYTHON3) services/app.py
 
 .PHONY: deps test
--- tests/model_test.py
+++ tests/model_test.py
@@ -1,33 +1,28 @@
 import unittest
 import sys
 
-from mongokit import Connection
+from peewee import *
+from playhouse.test_utils import test_database
 
 from services.model import User, Project
+from services.serializer import Serializer
 
-connection = Connection()
-connection.register([User, Project])
+test_db = SqliteDatabase('facehub.db')
 
 class UserTest(unittest.TestCase):
 
-    @unittest.skip("testing skipping")
-    def tearDown(self):
-        connection.test.drop_collection("projects")
-        connection.test.drop_collection("users")
+    def test_serializer(self):
+        with test_database(test_db, (User, Project)):
+            p = Project.create(name = 'Google Research')
+            User.create(name = "Jeff Dean",
+                        title = "Technical Follow",
+                        email = "jeff@google.com",
+                        project = p,
+                        avatar = "http://davidx.qiniudn.com/liuliang_head@2x.jpg",
+                        photo = "http://davidx.qiniudn.com/liuliang_half@2x.jpg",
+                        phone = "13060245883")
 
-    @unittest.skip("testing skipping")
-    def test_creation(self):
-        project = connection.test.Project()
-        project['name'] = "rea myfun"
-        project.save()
+            u = User.get(User.name == "Jeff Dean")
 
-        user = connection.test.User()
-        user['name'] = u'Michael'
-        user['title'] = u'Product Manager'
-        user['project'] = project
-        user['email'] = u'michael@rea-group.com'
-        user.save()
-
-        saved_user = connection.test.User.find_one({'name': u'Michael'})
-        self.assertEqual(saved_user['name'], u'Michael')
-        self.assertEqual(saved_user['project']['name'], u'rea myfun')
+            self.assertEqual(u.email, "jeff@google.com")
+            self.assertEqual(u.project.name, "Google Research")
