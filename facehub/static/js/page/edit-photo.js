df93274567de962ea6d365a5afc07fbed19edade
rbian <psychebb@163.com>
2015-02-13 10:39:59 +0800

    remove the useless code

--- facehub/static/js/page/edit-photo.js
+++ facehub/static/js/page/edit-photo.js
@@ -1,15 +1,8 @@
 (function() {
     var updatePreview = function(coordinate) {
-        $('.preview').removeClass('blank');
-        var rx = 300 / coordinate.w;
-        var ry = 400 / coordinate.h;
-        $('.preview img').css({
-            width: Math.round(rx * 680) + 'px',
-            // height: Math.round(ry * 370) + 'px',
-            marginLeft: '-' + Math.round(rx * coordinate.x) + 'px',
-            marginTop: '-' + Math.round(ry * coordinate.y) + 'px',
-        });
-        $('.preview img').data('coordinate', coordinate)
+        $('.preview img').data('coordinate', coordinate);
+        $('.preview img').hide();
+
     };
 
     var cropSelected = function() {
