4b698b6621b13a072a23bbac910cf63c8f7009ad
He Fei <droxer.he@gmail.com>
2015-03-19 16:11:01 +0800

    try to fix build.

--- tests/model_test.py
+++ tests/model_test.py
@@ -4,7 +4,7 @@ import sys
 from peewee import *
 from playhouse.test_utils import test_database
 
-from facehub.model import User, Project
+from facehub.model import User
 from facehub.serializer import Serializer
 
 test_db = SqliteDatabase('facehub.db')
@@ -12,12 +12,11 @@ test_db = SqliteDatabase('facehub.db')
 class UserTest(unittest.TestCase):
 
     def test_serializer(self):
-        with test_database(test_db, (User, Project)):
-            p = Project.create(name = 'Google Research')
+        with test_database(test_db, [User]):
             User.create(name = "Jeff Dean",
                         title = "Technical Follow",
                         email = "jeff@google.com",
-                        project = p,
+                        project = "Google Research",
                         avatar = "http://davidx.qiniudn.com/liuliang_head@2x.jpg",
                         photo = "http://davidx.qiniudn.com/liuliang_half@2x.jpg",
                         phone = "13060245883")
@@ -25,4 +24,4 @@ class UserTest(unittest.TestCase):
             u = User.get(User.name == "Jeff Dean")
 
             self.assertEqual(u.email, "jeff@google.com")
-            self.assertEqual(u.project.name, "Google Research")
+            self.assertEqual(u.project, "Google Research")
