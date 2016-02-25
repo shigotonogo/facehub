c82c957a54babf50f43510e362766e48f89c2508
Rui <rui.bian@rea-group.com>
2016-02-25 13:56:47 +0800

    set default date for birthday

--- facehub/static/js/page/profile.js
+++ facehub/static/js/page/profile.js
@@ -1,7 +1,8 @@
 (function() {
     $("#birthday").datepicker({
         changeMonth: true,
-        changeYear: true
+        changeYear: true,
+        defaultDate: new Date(1990,1,1)
     });
     $("#onboard").datepicker({
         changeMonth: true,
