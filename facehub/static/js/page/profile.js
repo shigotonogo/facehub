b1a0d63cca08240cf4efd21b821cc4a423d8532c
haizhou chen <hzchen@thoughtworks.com>
2016-04-13 17:25:22 +0800

    added range for datepicker

--- facehub/static/js/page/profile.js
+++ facehub/static/js/page/profile.js
@@ -2,10 +2,13 @@
     $("#birthday").datepicker({
         changeMonth: true,
         changeYear: true,
-        defaultDate: new Date(1990,1,1)
+        defaultDate: new Date(1990,1,1),
+        maxDate: 0
     });
     $("#onboard").datepicker({
         changeMonth: true,
-        changeYear: true
+        changeYear: true,
+        minDate: '01/01/1993',
+        maxDate: 0
     });
 })();
